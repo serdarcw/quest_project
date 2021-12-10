9f20dcf54d657cbf58071ccb65d2e08e6a02e0c9
serdarcw <serkangumus06@gmail.com>
2021-12-09 23:54:10 -0600

    new changes

--- Terraform/userdata.sh
+++ Terraform/userdata.sh
@@ -7,8 +7,7 @@ systemctl enable docker
 usermod -a -G docker ec2-user
 newgrp docker
 cd /home/ec2-user/
-TOKEN="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"
-git clone https://$TOKEN@github.com/serdarcw/quest_project.git
+git clone https://github.com/serdarcw/quest_project.git
 cd quest_project
 docker build -t serkan/quest:latest .
 docker run -p 80:3000 -d  --env SECRET_WORD=everything-starts-with-i-can-do serkan/quest:latest
